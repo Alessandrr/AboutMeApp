1e882ee5f828005beaca18f59e535f72dcba01bf
Alessandrr <107347838+Alessandrr@users.noreply.github.com>
2023-05-15 11:16:06 +0400

    Initial commit

---
+++ AboutMeApp/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  AboutMeApp
+//
+//  Created by Aleksandr Mamlygo on 15.05.23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+
+}
+
